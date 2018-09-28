c162af19b42bea00fd156c6875ee15dfabb851b8
CK Kashyap <ckk@microsoft.com>
2018-09-27 20:07:05 -0700

    use mock

---
+++ packages/botframework-directlinejs/__tests__/createConversationUsingMock.ts
@@ -0,0 +1,39 @@
+import { AjaxResponse, AjaxRequest } from 'rxjs/observable/dom/AjaxObservable';
+import { ConnectionStatus, DirectLine } from '../src/directLine';
+import { Observable } from 'rxjs/Observable';
+
+const conversationId = ""+Math.random();
+
+function getObject() {
+var r = {response: {conversationId: conversationId}} as AjaxResponse
+    console.log(r.response)
+    return r
+}
+
+var MyOb = Observable.create((s) => {
+    s.next(getObject())
+    s.complete()
+})
+
+describe('Create conversation should set conversation ID', async () => {
+it("hello", () => {
+
+    const spy = jest.spyOn(Observable,"ajax").mockReturnValue(MyOb)
+
+
+  try {
+    var directLine = new DirectLine({
+      domain: `http://localhost/v3/directline`,
+      webSocket: false
+    });
+
+
+    const subscription = directLine.activity$.subscribe(() => {});
+
+    expect(directLine).toHaveProperty('conversationId', 100);
+
+    subscription.unsubscribe();
+  } finally {
+  }
+  })
+});
